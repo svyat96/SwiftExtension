b1d7bc47ecfc33973990d2390343a1f601fa3904
sporykhin.s <svyat1996@gmail.com>
2022-03-20 13:04:09 +0300

    Init Project

---
+++ SwiftExtensions/SwiftExtensions/SwiftExtensions.h
@@ -0,0 +1,18 @@
+//
+//  SwiftExtensions.h
+//  SwiftExtensions
+//
+//  Created by Святослав Спорыхин on 3/20/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftExtensions.
+FOUNDATION_EXPORT double SwiftExtensionsVersionNumber;
+
+//! Project version string for SwiftExtensions.
+FOUNDATION_EXPORT const unsigned char SwiftExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExtensions/PublicHeader.h>
+
+
