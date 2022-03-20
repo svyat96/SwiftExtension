37c37b713e03e28dd29577bf23fc87b5304b738e
sporykhin.s <svyat1996@gmail.com>
2022-03-20 13:23:06 +0300

    Add first extension

---
+++ SwiftExtensions/SwiftExtensions/StringExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  StringExtensions.swift
+//  SwiftExtensions
+//
+//  Created by Святослав Спорыхин on 3/20/22.
+//
+
+import Foundation
+
+extension String {
+	public static let empty = ""
+	public static let space = " "
+	
+	public func add(_ value: CustomStringConvertible) -> String {
+		return self + value.description
+	}
+	
+	public func add(_ value: CustomDescription) -> String {
+		return self + value.customDescription
+	}
+}
+
+public protocol CustomDescription {
+	var customDescription: String { get }
+}
